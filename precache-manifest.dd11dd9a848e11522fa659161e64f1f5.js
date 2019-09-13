3b00be3f32fab5b90e983b1f39ffb1006eb27624
martinatrajk <trajkovicmartina94@gmail.com>
2019-09-13 21:47:28 +0200

    Updates

---
+++ precache-manifest.dd11dd9a848e11522fa659161e64f1f5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d654affc08c7e428a507dc0fbb8e8ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55b4a5de7462d7774ed8",
+    "url": "/static/css/main.ea71eaf7.chunk.css"
+  },
+  {
+    "revision": "56c92781c6453d70ddd1",
+    "url": "/static/js/2.7522523f.chunk.js"
+  },
+  {
+    "revision": "55b4a5de7462d7774ed8",
+    "url": "/static/js/main.0eaa8520.chunk.js"
+  },
+  {
+    "revision": "7c2eab6d59a1de4e4e6d",
+    "url": "/static/js/runtime~main.3d691157.js"
+  },
+  {
+    "revision": "d2f422e55454d2a89368c2fb62291bbe",
+    "url": "/static/media/INSTA.d2f422e5.png"
+  },
+  {
+    "revision": "2c22d30ba1d705a7f7eb2cf335198fa4",
+    "url": "/static/media/about.2c22d30b.jpg"
+  },
+  {
+    "revision": "7c1fd99f7aec267b09e10da9e9d61ac2",
+    "url": "/static/media/header.7c1fd99f.png"
+  },
+  {
+    "revision": "d11dbd70adf32887664bf0460e9b7123",
+    "url": "/static/media/user.d11dbd70.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/RecipeApp/precache-manifest.fb6802b96b63d67032edf46d6aff700f.js"
+  "/precache-manifest.dd11dd9a848e11522fa659161e64f1f5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/RecipeApp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
