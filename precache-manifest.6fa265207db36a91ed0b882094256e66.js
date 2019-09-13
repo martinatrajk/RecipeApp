00fd51c60e6edf740561d46a3c864392a8bfdfa6
martinatrajk <trajkovicmartina94@gmail.com>
2019-09-13 21:24:08 +0200

    Updates

---
+++ precache-manifest.6fa265207db36a91ed0b882094256e66.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62a561f59aa57326d6cc802af0451463",
+    "url": "/martinatrajk/RecipeApp/index.html"
+  },
+  {
+    "revision": "669efd0b6b5e16507587",
+    "url": "/martinatrajk/RecipeApp/static/css/main.ea71eaf7.chunk.css"
+  },
+  {
+    "revision": "56c92781c6453d70ddd1",
+    "url": "/martinatrajk/RecipeApp/static/js/2.7522523f.chunk.js"
+  },
+  {
+    "revision": "669efd0b6b5e16507587",
+    "url": "/martinatrajk/RecipeApp/static/js/main.88d44b71.chunk.js"
+  },
+  {
+    "revision": "069142b2e574d5fca396",
+    "url": "/martinatrajk/RecipeApp/static/js/runtime~main.f7a220a6.js"
+  },
+  {
+    "revision": "d2f422e55454d2a89368c2fb62291bbe",
+    "url": "/martinatrajk/RecipeApp/static/media/INSTA.d2f422e5.png"
+  },
+  {
+    "revision": "2c22d30ba1d705a7f7eb2cf335198fa4",
+    "url": "/martinatrajk/RecipeApp/static/media/about.2c22d30b.jpg"
+  },
+  {
+    "revision": "7c1fd99f7aec267b09e10da9e9d61ac2",
+    "url": "/martinatrajk/RecipeApp/static/media/header.7c1fd99f.png"
+  },
+  {
+    "revision": "d11dbd70adf32887664bf0460e9b7123",
+    "url": "/martinatrajk/RecipeApp/static/media/user.d11dbd70.png"
+  }
+]);
